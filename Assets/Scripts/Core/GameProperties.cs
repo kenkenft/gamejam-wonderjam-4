6b2d50329a67491b570a4fba0112ae9609c3e522
Ken Tang <kenftang@gmail.com>
2023-03-11 11:11:50 +0000

    Modify GameProperties
    Removed OnEnable method as the class is not a
    csriptable object type.

--- Assets/Scripts/Core/GameProperties.cs
+++ Assets/Scripts/Core/GameProperties.cs
@@ -11,20 +11,6 @@ public class GameProperties
                                                                                                                     {"l", Resources.LoadAll<CannonBall>("Large")},
                                                                                                                     {"x", Resources.LoadAll<CannonBall>("XLarge")}
                                                                                                                 };
-    public void OnEnable()
-    {
-        
-        CannonBallTypeDict.Add("s", Resources.LoadAll<CannonBall>("Small"));
-        CannonBallTypeDict.Add("m", Resources.LoadAll<CannonBall>("Medium"));
-        CannonBallTypeDict.Add("l", Resources.LoadAll<CannonBall>("Large"));
-        CannonBallTypeDict.Add("x", Resources.LoadAll<CannonBall>("XLarge"));
-
-        // Debug.Log("Small pool type size: " + _cannonBallTypeDict["s"].Length);
-        // Debug.Log("Medium pool type size: " + _cannonBallTypeDict["m"].Length);
-        // Debug.Log("Large pool type size: " + _cannonBallTypeDict["l"].Length);
-        // Debug.Log("XLarge pool type size: " + _cannonBallTypeDict["x"].Length);
-        
-    }
     
     public static CannonBall GetCannonBallType(string size, int typeIDNum)
     {
