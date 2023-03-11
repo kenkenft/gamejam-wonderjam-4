a95ae15eea4d35dffd242a8b61d50651b8a64519
Ken Tang <kenftang@gmail.com>
2023-03-11 10:55:20 +0000

    Modify CannonManager

--- Assets/Scripts/CannonManager.cs
+++ Assets/Scripts/CannonManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CannonManager : MonoBehaviour
 {
     private CannonBallLoadingManager _cannonBallLoadingManager;  
+    [SerializeField] private CannonProperties _cannonProperties;
     [SerializeField] private int _gameState; // 0 is start or initial setup; 1 is replenishment; 2 is loading; 3 is aiming; 4 is firing; 5 is update cannonballs and enemies; 6 is endgame checks
 
     void Start()
@@ -16,6 +17,8 @@ public class CannonManager : MonoBehaviour
         _gameState = 0;
         _cannonBallLoadingManager = GetComponentInChildren<CannonBallLoadingManager>();
         _cannonBallLoadingManager.SetUpLoadingPhase();
+
+        _cannonProperties = GetComponentInChildren<CannonProperties>();
     }
 
     public void SetGameState(int state)
