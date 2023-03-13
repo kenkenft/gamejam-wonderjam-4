73dd9019d6253b3163d91e7abc355e3eb5943ca1
Ken Tang <kenftang@gmail.com>
2023-03-13 15:30:16 +0000

    Modify EnemyTileManager
    EnemyTileManager now stores the created tile instances
    in a dictionary, EnemyTileDict. The key is the row value,
    and the associated value is a list of the row of tiles.
    
    SetEnemyTileIsTargetable sets the _isTargetable property
    of the enemy tiles. This is called when phaseState is set
    to 3 or 4.

--- Assets/Scripts/EnemyTileManager.cs
+++ Assets/Scripts/EnemyTileManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class EnemyTileManager : MonoBehaviour
 {
-    public GameObject Tile;     // Prefab of Tile and Connector pieces
+    public GameObject Tile;     // Prefab of Tile
+    public CannonProperties CannonInstance;
+    [SerializeField] public Dictionary<int, List<GameObject>> EnemyTileDict = new Dictionary<int, List<GameObject>>();
     public int BoardHeight, BoardWidth;
     
     private float _tileMargin = 1.20f;       // Percentage margin of extra space around tile
@@ -27,36 +29,47 @@ public class EnemyTileManager : MonoBehaviour
 
     public void CreateTiles(float tileOffsetX, float tileOffsetY) 
     {
-        //
-        // tiles = new GameObject[boardHeight, boardWidth];
 
         // At the moment, this will set the first tile at the center of the board gameObject instead of the corner of the board gameObject
         float startX = transform.position.x;    
         float startY = transform.position.y;
 
+        // List<GameObject> tempList = new List<GameObject>();
         // Start of for loop for setting tile objects' positions and properties
         for (int row = 0; row < BoardHeight; row++) 
         {      
+            List<GameObject> tempList = new List<GameObject>();
             for (int col = 0; col < BoardWidth; col++) 
             {
                 GameObject newTile = Instantiate(Tile, new Vector3(startX + (tileOffsetX * col), startY - (tileOffsetY * row), 0), Tile.transform.rotation);
-                       
-                // tiles[row, col] = newTile;
+                tempList.Add(newTile);
                 newTile.transform.parent = transform;
-                SetTileProperties(newTile, row, col); // Set tile properties e.g. tile id, element, tile sprite 
-                // Debug.Log("row: " + row + ", col: " + col + ", tiledID: " + newTile.GetComponent<TileProperties>().GetTileID());
+                SetTileProperties(newTile, col, row); // Set tile properties 
             }
+            EnemyTileDict.Add(row, tempList);
         } // End of for loop for placing tile objects
     }   ////// End of CreateTiles()
 
-    public void SetTileProperties(GameObject targetTile, int row, int col)
+    public void SetTileProperties(GameObject targetTile, int col, int row)
     {
         TileProperties tileClass = targetTile.GetComponent<TileProperties>();
         // Set the individual tile's properties
-        
-        // tileClass.SetTileID(col + (boardHeight * row));  // Causes autotest failure
-        targetTile.name = tileClass.GetTileCoordinates()[0].ToString() + tileClass.GetTileCoordinates()[1].ToString();
+        targetTile.name = col.ToString() + row.ToString();
+        tileClass.SetTileCoordinates(col, row);
+        tileClass.SetIsTargetable(false);
+        tileClass.SetCannonRef(CannonInstance);
     }   ////// End of SetTileProperties()
+
+    void SetEnemyTileIsTargetable(bool state)
+    {
+        for(int row = 0; row < EnemyTileDict.Count; row ++)
+        {
+            for(int col = 0; col < EnemyTileDict[row].Count; col ++)
+            {
+                EnemyTileDict[row][col].GetComponent<TileProperties>().SetIsTargetable(state);
+            }
+        }
+    }
 
     void CheckGameState(int gameState)
     {
@@ -68,6 +81,17 @@ public class EnemyTileManager : MonoBehaviour
                 Debug.Log("gameState is 1! Calling SetUpLoadingPhase");
                 SetUpBoard();
                 CannonManager.SetGameState(2);
+                break;
+            }
+            case 3:
+            {
+                //Turn off enemy tiles
+                SetEnemyTileIsTargetable(false);
+                break;
+            }
+            case 4:
+            {
+                SetEnemyTileIsTargetable(true);
                 break;
             }
             default:
